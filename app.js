aa3dd493fec036c74c4b96657133b62b231252d3
Nasseima <lindsaynasseima@gmail.com>
2024-05-17 15:31:35 -0400

    Data colections project

---
+++ app.js
@@ -0,0 +1,27 @@
+// Refactoring old Code 
+//function oldCode(object, key){
+//     const code = [{index: "1", mass: "0.00", spring1:".050", spring2: ".050"},
+//                 {index: "2", mass: "0.49", spring1:"0.066", spring2: "0.066"},
+//                 {index: "3", mass: "0.98", spring1:"0.087", spring2: "0.080"},
+//                 {index: "4", mass: "1.47", spring1:"0.116", spring2: "0.108"},
+//                 {index: "5", mass: "1.96", spring1:"0.142", spring2: "0.138"},
+//                 {index: "6", mass: "2.45", spring1:"0.166", spring2: "0.158"},
+//                 {index: "7", mass: "2.94", spring1:"0.193", spring2: "0.174"},
+//                 {index: "8", mass: "3.43", spring1:"0.204", spring2: "0.192"},
+//                 {index: "9", mass: "3.92", spring1:"0.226", spring2: "0.205"},
+//                 {index: "10", mass: "4.41", spring1:"0.238", spring2: "0.232"},
+// ]
+
+// let codeCopy = code.concat(index)
+// console.log(code)
+
+let string1 = ["1", "0.00", ".050", ".050"];
+let spring2 = ["2", "0.49", "0.066", "0.066"]
+let spring3 =["3", "0.98", "0.087", "0.080"]
+let spring4 =["4", "1.47", "0.116", "0.108"]
+let spring5 =["5", "1.96", "0.142", "0.138"]
+let spring6 =["6", "2.45", "0.166", "0.158"]
+let spring7 =["7", "2.94", "0.193", "0.174"]
+let spring8 =["8", "3.43", "0.204", "0.192"]
+let spring9 =["9", "3.92", "0.226", "0.205"]
+let spring10 =["10", "4.41", "0.238", "0.232"]
